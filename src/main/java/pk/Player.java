edc072a1272a09a478a401496942699a9bc1e48c
Omar Shehada <awadomar24@gmail.com>
2023-01-13 20:18:15 -0500

    Created play method

--- src/main/java/pk/Player.java
+++ src/main/java/pk/Player.java
@@ -1,5 +1,8 @@
 package pk;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Player {
 
     Dice[] playerDice = {new Dice(), new Dice(), new Dice(),new Dice(),new Dice(),new Dice(),new Dice(),new Dice()};
@@ -13,11 +16,37 @@ public class Player {
         return roll;
     }
 
+    public void play(){
+        int skulls = 0;
+        int score = 0;
+        int dag = 0; //dag = diamonds and gold
+        Faces[] roll = roll8();
+
+        do{
+            for(int i = 0; i < 8; i++){
+                skulls = roll[i] == Faces.SKULL ? skulls+1 : skulls;
+            }
+            if(skulls > 3){
+                break;
+            }
+
+
+            Random rnd = new Random();
+
+            //dag = (roll[i] == Faces.DIAMOND | roll[i] == Faces.GOLD) ? dag+1 : dag;
+
+
+        }
+        while (true);
+
+    }
+
     public static void main(String[] args) {
         Player pl = new Player();
         for(int i = 0; i < 8; i++){
             System.out.println(pl.roll8()[i]);
         }
+        pl.play();
     }
 
 
